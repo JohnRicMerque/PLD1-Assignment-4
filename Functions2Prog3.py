e8a4e65bfce9d56bb3d4bd3e18c3c27454bf8928
unknown <you@example.com>
2021-11-07 18:52:45 +0800

    Program complete: functions return multiple values

--- Functions2Prog3.py
+++ Functions2Prog3.py
@@ -5,5 +5,16 @@ def getMoneyAplprice():
     aplPrice = float(input("Enter the price of one apple: "))
     return moneyAmount, aplPrice
 
+def getMaxApples(money, price):
+    maxApls = int(money//price)
+    return maxApls
+
+def getChange(money, price):
+    change = money%price
+    return change
+
 amountOfMoney, applePrice = getMoneyAplprice()
-print(f"money = {amountOfMoney} and price = {applePrice}")
+max_apples = getMaxApples(amountOfMoney, applePrice)
+total_change = getChange(amountOfMoney, applePrice)
+
+print(f"You can buy {max_apples} apples and your change is {total_change} pesos.")
